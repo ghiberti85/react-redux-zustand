472912f3b9e9a22d68531fc62318e81f95950009
ghiberti85 <ghiberti85@gmail.com>
2024-04-22 23:26:55 -0300

    Module Structure

--- src/pages/Player.tsx
+++ src/pages/Player.tsx
@@ -1,4 +1,4 @@
-import { MessageCircle } from 'lucide-react'
+import { ChevronDown, MessageCircle } from 'lucide-react'
 
 export function Player () {
     return (
@@ -20,7 +20,20 @@ export function Player () {
                     <div className="flex-1">
                         video
                     </div>
-                    <aside className="w-80 border-l border-zinc-800 bg-zinc-900 h-[600px]"></aside>
+                    <aside className="w-80 border-l border-zinc-800 bg-zinc-900 h-[600px]">
+                        <div>
+                            <button className="flex w-full items-center gap-3 bg-zinc-800 p-4">
+                                <div className="flex h-10 w-10 rounded-full items-center justify-center bg-zinc-950 text-xs">
+                                    1
+                                </div>
+                                <div className="flex flex-col gap-1 text-left">
+                                    <strong className="text-sm">Desvendando o Redux</strong>
+                                    <span className="text-xs text-zinc-400">12 aulas</span>
+                                </div>
+                                <ChevronDown className="w-5 h-5 ml-auto text-zinc-400" />
+                            </button>
+                        </div>
+                    </aside>
                 </main>
             </div>
         </div>
