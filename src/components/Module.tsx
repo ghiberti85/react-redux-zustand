587313e4cf81e8dcdc128a7b5617d14d0c968d68
ghiberti85 <ghiberti85@gmail.com>
2024-04-26 14:29:04 -0300

    Selecting current class

--- src/components/Module.tsx
+++ src/components/Module.tsx
@@ -3,6 +3,8 @@ import * as Collapsible from '@radix-ui/react-collapsible';
 import { ChevronDown } from "lucide-react";
 import { Lesson } from "./Lesson";
 import { useAppSelector } from '../store';
+import { useDispatch } from 'react-redux';
+import { play } from '../store/slices/player';
 
 interface ModuleProps {
     moduleIndex: number
@@ -11,9 +13,11 @@ interface ModuleProps {
 }
 
 export function Module({ moduleIndex, title, amountOfLessons }: ModuleProps) {
+    const dispatch = useDispatch()
+
     const lessons = useAppSelector((state) => {
         return state.player.course.modules[moduleIndex].lessons
-    })
+    }) 
     return (
         <Collapsible.Root className="group">
             <Collapsible.Trigger className="flex w-full items-center gap-3 bg-zinc-800 p-4">
@@ -22,18 +26,19 @@ export function Module({ moduleIndex, title, amountOfLessons }: ModuleProps) {
                 </div>
                 <div className="flex flex-col gap-1 text-left">
                     <strong className="text-sm">{title}</strong>
-                    <span className="text-xs text-zinc-400">{amountOfLessons}</span>
+                    <span className="text-xs text-zinc-400">{amountOfLessons} aulas</span>
                 </div>
                 <ChevronDown className="w-5 h-5 ml-auto text-zinc-400 group-data-[state=open]:rotate-180 transition-transform" />
             </Collapsible.Trigger>
             <Collapsible.Content>
                 <nav className="relative flex flex-col gap-4 p-6">
-                    {lessons.map(lesson => {
+                    {lessons.map((lesson, lessonIndex) => {
                         return (
                             <Lesson
                                 key={lesson.id}
                                 title={lesson.title} 
-                                duration={lesson.duration} 
+                                duration={lesson.duration}
+                                onPlay={() => dispatch(play([moduleIndex, lessonIndex]))}
                             />
                         )
                     })}
