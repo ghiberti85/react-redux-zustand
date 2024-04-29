9a36e8c8cfd5ddbe18e2d25afc6cb07f58d5f6bd
ghiberti85 <ghiberti85@gmail.com>
2024-04-29 00:08:42 -0300

    Loading course data

--- src/pages/Player.tsx
+++ src/pages/Player.tsx
@@ -3,10 +3,14 @@ import { Header } from '../components/Header'
 import { Video } from '../components/Video'
 import { Module } from '../components/Module'
 import { useAppSelector } from '../store'
-import { useCurrentLesson } from '../store/slices/player'
+import { start, useCurrentLesson } from '../store/slices/player'
 import { useEffect } from 'react'
+import { api } from '../lib/axios'
+import { useDispatch } from 'react-redux'
 
 export function Player () {
+    const dispatch = useDispatch()
+    
     const modules = useAppSelector(state => {
         return state.player.course.modules
     })
@@ -14,7 +18,15 @@ export function Player () {
     const { currentLesson } = useCurrentLesson()
 
     useEffect(() => {
-        document.title = `Assistindo: ${currentLesson.title}`
+        api.get('/courses/1').then(response => {
+            dispatch(start(response.data))
+          })
+        }, []);
+      
+        useEffect(() => {
+          if (currentLesson) {
+            document.title = `Assistindo: ${currentLesson.title}`
+          }
     }, [currentLesson])
 
     return (
@@ -35,7 +47,7 @@ export function Player () {
 
                     </div>
                     <aside className="w-80 absolute top-0 bottom-0 right-0 border-l divide-y-2 divide-zinc-900 border-zinc-800 bg-zinc-900s overflow-y-scroll scrollbar scrollbar-track-zinc-950 scrollbar-thumb-zinc-800" >
-                        { modules.map((module, index) => {
+                        { modules && modules.map((module, index) => {
                             return (
                                 <Module
                                     key={module.id}
