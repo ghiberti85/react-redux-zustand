047d4d9cbcab861c4275774c22e1fe5b9793f598
ghiberti85 <ghiberti85@gmail.com>
2024-04-29 22:17:04 -0300

    Using Async Thunks

--- src/store/slices/player.ts
+++ src/store/slices/player.ts
@@ -1,5 +1,6 @@
-import { PayloadAction, createSlice } from '@reduxjs/toolkit'
+import { PayloadAction, createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 import { useAppSelector } from '..';
+import { api } from "../../lib/axios";
 
 interface Course {
   id: number
@@ -25,6 +26,15 @@ const initialState: PlayerState = {
   currentModuleIndex: 0,
   currentLessonIndex: 0,
 }
+
+export const loadCourse = createAsyncThunk(
+  'player/load',
+  async () => {
+    const response = await api.get('/courses/1')
+
+    return response.data
+  }
+)
 
 export const playerSlice = createSlice({
     name: 'player',
@@ -54,10 +64,15 @@ export const playerSlice = createSlice({
         }
       }
     },
+    extraReducers(builder) {
+      builder.addCase(loadCourse.fulfilled, (state, action) => {
+        state.course = action.payload
+      })
+    },
 })
 
 export const player = playerSlice.reducer
-export const { play, next, start } = playerSlice.actions
+export const { play, next } = playerSlice.actions
 
 export const useCurrentLesson = () => {
   return useAppSelector(state => {
