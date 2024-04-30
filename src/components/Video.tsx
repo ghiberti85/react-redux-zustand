824e0ff4c77359fce15fc9110641e65ba920c6aa
ghiberti85 <ghiberti85@gmail.com>
2024-04-29 22:25:02 -0300

    Creating Loading Interface

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,11 +1,12 @@
 import ReactPlayer from "react-player";
-import { useAppDispatch } from "../store";
+import { useAppDispatch, useAppSelector } from "../store";
+import { Loader } from "lucide-react";
 import { useCurrentLesson } from "../store/slices/player";
 
 export function Video() {
     const dispatch = useAppDispatch()
-
     const {currentLesson } = useCurrentLesson()
+    const isCourseLoading = useAppSelector(state => state.player.isLoading)
 
     function handlePlayNext() {
         dispatch(next())
@@ -15,13 +16,20 @@ export function Video() {
 
     return (
         <div className="w-full bg-zinc-950 aspect-video">
-            <ReactPlayer 
+            {isCourseLoading ? (
+                <div className="flex h-full items-center justify-center">
+                <Loader className="w-6 h-6 text-zinc-400 animate-spin" />
+                </div>
+            ) : (
+                <ReactPlayer
                 width="100%"
                 height="100%"
                 controls
+                playing
                 onEnded={handlePlayNext}
-                url={`https://www.youtube.com/watch?v=${currentLesson.id}`}
-            />
+                url={`https://www.youtube.com/watch?v=${currentLesson?.id}`}
+                />
+            )}
         </div>
     )
 }
